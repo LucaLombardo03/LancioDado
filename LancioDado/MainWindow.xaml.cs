986e071c542e6614b026c5dc7d7f6073147a5728
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-06 11:44:10 +0100

    grafica

--- LancioDado/MainWindow.xaml.cs
+++ LancioDado/MainWindow.xaml.cs
@@ -29,59 +29,6 @@ namespace LancioDado
             TxtDado.MaxLength = 1;
         }
         Random random = new Random();
-        private void Lancio_Click(object sender, RoutedEventArgs e)
-        {
-            int da = 1;
-            int a = 6;
-            int numeroCasuale = random.Next(da, a);
-            int crediti = int.Parse(TxtCrediti.Text);
-            if (crediti <= 0)
-                MessageBox.Show("INSERISCI UN NUMERO DI CREDITI POSITIVO");
-            try
-            {
-                int n = int.Parse(TxtDado.Text);
-                if (n >= 7 || n <= 0)
-                {
-                    MessageBox.Show("INSERISCI UN NUMERO DA 1 A 6!!!!");
-                }
-                else if (numeroCasuale == n)
-                {
-                    TxtRisultato.Text = ($"Il numero uscito è {numeroCasuale}. Complimenti!Hai vinto!");
-                    crediti += 40;
-                    TxtCrediti.Text = ($"{crediti}");
-                }
-                else if (numeroCasuale != n)
-                {
-                    TxtRisultato.Text = ($"Il numero uscito è {numeroCasuale}. Ritenta!");
-                    crediti -= 10;
-                    TxtCrediti.Text = ($"{crediti}");
-                }
-                dado.Source = new BitmapImage(new Uri($@"Dadi\dado{numeroCasuale}.png", UriKind.Relative));
-            }
-            catch (Exception)
-            {
-                MessageBox.Show($"Il numero uscito è {numeroCasuale}. Ritenta!");
-            }
-
-        }
-        private void Ricomincia_Click(object sender, RoutedEventArgs e)
-        {
-            TxtDado.Clear();
-            TxtRisultato.Clear();
-            TxtCrediti.Clear();
-            dado.Source = null;
-        }
-
-        private void Lancio_Click(object sender, TouchEventArgs e)
-        {
-
-        }
-
-        private void Lancio_Click(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
         {
             int da = 1;
@@ -102,20 +49,27 @@ namespace LancioDado
                     TxtRisultato.Text = ($"Il numero uscito è {numeroCasuale}. Complimenti!Hai vinto!");
                     crediti += 40;
                     TxtCrediti.Text = ($"{crediti}");
+                    dado.Source = new BitmapImage(new Uri($@"Dadi\dado{numeroCasuale}.png", UriKind.Relative));
                 }
                 else if (numeroCasuale != n)
                 {
                     TxtRisultato.Text = ($"Il numero uscito è {numeroCasuale}. Ritenta!");
                     crediti -= 10;
                     TxtCrediti.Text = ($"{crediti}");
+                    dado.Source = new BitmapImage(new Uri($@"Dadi\dado{numeroCasuale}.png", UriKind.Relative));
                 }
-                dado.Source = new BitmapImage(new Uri($@"Dadi\dado{numeroCasuale}.png", UriKind.Relative));
             }
             catch (Exception)
             {
                 MessageBox.Show($"Il numero uscito è {numeroCasuale}. Ritenta!");
             }
-
+        }
+        private void Image_MouseUp_1(object sender, MouseButtonEventArgs e)
+        {
+            TxtDado.Clear();
+            TxtRisultato.Clear();
+            TxtCrediti.Clear();
+            dado.Source = null;
         }
     }
 }
